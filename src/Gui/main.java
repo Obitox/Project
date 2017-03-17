69d7799740edd3c5479b10496cb227965a40b34f
test <test>
2017-03-17 12:15:26 +0100

    TestBojan

---
+++ src/Gui/main.java
@@ -0,0 +1,10 @@
+package Gui;
+
+/**
+ * Created by Vlatacom on 17.3.2017..
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Second test");
+    }
+}
