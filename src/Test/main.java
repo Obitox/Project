f497d8a3cb2d1df593ec483ed2671af26fd758bf
test <test>
2017-03-17 12:03:12 +0100

    testDrazen

--- src/Test/main.java
+++ src/Test/main.java
@@ -1,9 +1,13 @@
 package Test;
 
+import java.net.SocketPermission;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Ja sam car");
         System.out.println("Ja sam Blagoje");
+        System.out.println("Ja sam Drazen");
+
 
     }
 }
