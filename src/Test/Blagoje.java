cb842edb57a72e48aba51d2a3e95037f3a254605
test <test>
2017-03-17 11:43:32 +0100

    Frist test

---
+++ src/Test/Blagoje.java
@@ -0,0 +1,11 @@
+package Test;
+
+/**
+ * Created by Vlatacom on 17.3.2017..
+ */
+public class Blagoje {
+
+    public static void main(String[] args) {
+        System.out.println("Bljagoje");
+    }
+}
