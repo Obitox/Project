2abab6201ce97d3eafc62091bb6e66a6cda5b9e4
test <test>
2017-03-17 15:17:42 +0100

    Test Vojislav

---
+++ src/gui/House.java
@@ -0,0 +1,45 @@
+package gui;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Created by Vlatacom on 17.3.2017..
+ */
+public class House {
+    private int startPosition;
+    private int endPosition;
+    private Color color;
+    private Figure[] arrayOfFigures = new Figure[4];
+
+    public House(Color color) {
+        this.color = color;
+
+        if(color.equals(Color.RED)){
+            startPosition = 0;
+            endPosition = this.calcEndPosition();
+        } else if(color.equals(Color.BLUE)){
+            startPosition = 9;
+            endPosition = this.calcEndPosition();
+        } else if(color.equals(Color.GREEN)){
+            startPosition = 19;
+            endPosition = this.calcEndPosition();
+        } else {
+            startPosition = 29;
+            endPosition = this.calcEndPosition();
+        }
+        for(int i=0; i<arrayOfFigures.length;i++)
+        {
+            arrayOfFigures[i]=new Figure(startPosition, color);
+        }
+    }
+
+    private int calcEndPosition(){
+        int temp = startPosition + 39;
+        if (temp > 39) {
+            temp = temp - 39;
+        }
+        return temp;
+    }
+    
+
+}
---
+++ src/gui/Parking.java
@@ -0,0 +1,8 @@
+package gui;
+
+/**
+ * Created by Vlatacom on 17.3.2017..
+ */
+public class Parking {
+
+}
