425a04371c37b9ef2769f643f53d186a8516f0ab
test <test>
2017-03-17 12:09:36 +0100

    testDrazen

--- src/Test/main.java
+++ src/Test/main.java
@@ -6,7 +6,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Ja sam car");
         System.out.println("Ja sam Blagoje");
-        System.out.println("Ja sam Drazen");
+        System.out.println("Ja sam Dra");
         System.out.println("Nenad TeSt");
 
 
