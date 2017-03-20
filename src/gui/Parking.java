e92d9a2b68c79de7ccf68583d28750c220c85f00
test <test>
2017-03-20 15:01:16 +0100

    Updated class "Parking" added: defColor for empty fields

--- src/gui/Parking.java
+++ src/gui/Parking.java
@@ -8,9 +8,19 @@ import javafx.scene.paint.Color;
 public class Parking {
     Field[] fields;
     Color color;
+    Color defaultColor;
 
     public Parking(Color color) {
         this.color = color;
+        if(color.equals(Color.RED)){
+            defaultColor = Color.rgb(255, 124, 124);
+        } else if(color.equals(Color.BLUE)){
+            defaultColor = Color.rgb(178, 178, 255);
+        } else if(color.equals(Color.GREEN)){
+            defaultColor = Color.rgb(168, 255, 168);
+        } else {
+            defaultColor = Color.LIGHTYELLOW;
+        }
 
 
     }
