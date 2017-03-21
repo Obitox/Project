983084e85985a2c93d843ef2b0c85623b09bbd01
test <test>
2017-03-21 11:28:28 +0100

    Dodati paketi server i communication i klasa Server

---
+++ src/server/Server.java
@@ -0,0 +1,14 @@
+package server;
+
+import javafx.application.Application;
+
+public class Server  implements Runnable{
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
