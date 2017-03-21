d65e75db304dc796a7e6b70d3f373dd4a2dad212
test <test>
2017-03-21 11:41:04 +0100

    Updated class "Parking" added: defColor for empty fields

--- src/network/DB.java
+++ src/network/DB.java
@@ -12,21 +12,14 @@ public class DB {
             Class.forName("com.mysql.jdbc.Driver");
             for (int i = 0; i < MAX_CON; i++) {
                 bafer[i] =
-                        DriverManager.getConnection("jdbc:mysql://localhost:3306/ludo", "root", "");
+                        DriverManager.getConnection("jdbc:mysql://192.168.99.109:3306/ludo", "root", "");
+                        System.out.println("Connection sucessful");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    /* za Access
-    private DB(){
-    try{
-    Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-    for(int i=0; i<MAX_CON; i++)
-    bafer[i] = DriverManager.getConnection("jdbc:odbc:dbkorisnik");
-    }catch(Exception e){}
-    }
-    */
+
     public static DB getInstance() {
         if (instance == null) {
             instance = new DB();
